6bd635cd70d6d38f53844c1c51f1f258889c7a1c
Cristian <cristian.maluquish@gmail.com>
2020-06-05 16:18:24 -0500

    Solucion

---
+++ src/clases/pelicula.class.ts
@@ -0,0 +1,45 @@
+
+export class Pelicula{
+    
+  
+    constructor(public nombre:string,public director:string, public estreno:Date,public duracion:number,public familiar:boolean){
+     
+    }
+    
+   
+
+    desplegar(){
+    
+
+        if(this.familiar){
+
+            console.log( ` ${this.nombre} fue dirigida por ${this.director} tiene una duración de ${this.convertirFormato()}, se entrenó en el año ${this.estreno.getFullYear()+1} es apta para toda la familia.`);
+            document.body.innerHTML+="<h1>"+`${this.nombre} fue dirigida por ${this.director} tiene una duración de ${this.convertirFormato()}, se entrenó en el año ${this.estreno.getFullYear()+1} es apta para toda la familia.`;
+         }
+         else{
+            console.log( `${this.nombre} fue dirigida por ${this.director} tiene una duración de ${this.convertirFormato()}, se entrenó en el año ${this.estreno.getFullYear()+1} y es para mayores de 18 años.`);
+            
+            document.body.innerHTML+="<h1>"+`${this.nombre} fue dirigida por ${this.director} tiene una duración de ${this.convertirFormato()}, se entrenó en el año ${this.estreno.getFullYear()+1} y es para mayores de 18 años.`;
+         }
+
+    
+
+         
+    }
+
+    convertirFormato():String{
+
+        var hours = Math.floor(this.duracion/ 60);  
+        var minutes = this.duracion % 60;
+        return hours + ":" + minutes;   
+
+    }
+
+}
+
+    
+
+
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import{Pelicula} from "./clases/pelicula.class"
+
+let pelicula1 = new Pelicula("La saga de Saw ","James Wan",new Date('2004'),103,false);
+
+
+pelicula1.desplegar();
+pelicula1.convertirFormato();
